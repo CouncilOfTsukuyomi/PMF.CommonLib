7da7e7ecf4b15d33d9a93ea1b57185688a865d45
Dylan <41908416+ErrorDodo@users.noreply.github.com>
2025-07-17 20:39:25 +0800

    Add new setting

--- CommonLib/Models/UIConfigurationModel.cs
+++ CommonLib/Models/UIConfigurationModel.cs
@@ -10,8 +10,10 @@ public class UIConfigurationModel
 
     [Display(Name = "Enable Notification Sound", GroupName = "Notification", Description = "Sound for Notifications")]
     public bool NotificationSoundEnabled { get; set; } 
-    [Display(Name = "Minimise To Tray", GroupName = "User Interface", Description = "Minimise the Main Window to tray")]
+    [Display(Name = "Minimise To Tray", GroupName = "User Interface", Description = "Minimise the application to tray")]
     public bool MinimiseToTray { get; set; } = false;
+    [Display(Name = "Minimise on Close", GroupName = "User Interface", Description = "Minimise the application when you click the X button")]
+    public bool MinimiseOnClose { get; set; } = false;
     [Display(Name = "Show Notification Button", GroupName = "User Interface", Description = "Show the Notification Button")]
     public bool ShowNotificationButton { get; set; } = true;
     [ExcludeFromSettingsUI]
