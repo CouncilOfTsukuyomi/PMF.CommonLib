a6e23bdf24d1c9ad15a28521facd4df37c7cbbd3
Gwen <oddball@oddball.tf>
2025-07-19 19:02:04 -0700

    Linux fixes pt2 (#8)
    * Linux fixes pt1
    
    * Linux Fixes Pt2
    
    Changes PenumbraService InstallMod to use System.IO.Compression instead of submodule extractor.

--- CommonLib/Consts/ConfigurationConsts.cs
+++ CommonLib/Consts/ConfigurationConsts.cs
@@ -2,40 +2,41 @@
 
 public static class ConfigurationConsts
 {
-    
     private static readonly string BaseDirectory = AppDomain.CurrentDomain.BaseDirectory;
 
     /// <summary>
-    /// This is where everything will go, inside configuration files, extracted files, queue saves
+    /// This is where everything will go, inside configuration files, extracted files, queue saves.
     /// </summary>
-    public static readonly string OldConfigPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\PenumbraModForwarder";
+    public static readonly string OldConfigPath = Path.Combine(
+        Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+        "PenumbraModForwarder");
     
     /// <summary>
-    /// This is where configuration options will be stored
+    /// This is where configuration options will be stored.
     /// Example:
     ///     Download Path - Where downloads will be found
     /// </summary>
-    public static readonly string ConfigurationFilePath = BaseDirectory + @"\config-v3.json"; // Change this to @"\config.json" when ready for prod
+    public static readonly string ConfigurationFilePath = Path.Combine(BaseDirectory, "config-v3.json");
     
     /// <summary>
-    /// The folder location where mods will be moved to after found inside the download folder
-    /// This is so we can do nice cleanups and users will have a spot to find all mods that have been downloaded
+    /// The folder location where mods will be moved to after found inside the download folder.
+    /// This is so we can do nice cleanups and users will have a spot to find all mods that have been downloaded.
     /// Maybe have a history.json as well?
     /// </summary>
-    public static readonly string ModsPath = BaseDirectory + @"\mods\";
+    public static readonly string ModsPath = Path.Combine(BaseDirectory, "mods");
     
     /// <summary>
     /// The folder location where logs will be found
     /// </summary>
-    public static readonly string LogsPath = BaseDirectory + @"\logs\";
+    public static readonly string LogsPath = Path.Combine(BaseDirectory, "logs");
     
-    public static readonly string DatabasePath = BaseDirectory + @"\database\";
+    public static readonly string DatabasePath = Path.Combine(BaseDirectory, "database");
     
-    public static readonly string FileWatcherState = BaseDirectory + @"\state\";
+    public static readonly string FileWatcherState = Path.Combine(BaseDirectory, "state");
     
     /// <summary>
-    /// The user should never be able to set what this is
-    /// This is where we cache the mods for the homeview
+    /// The user should never be able to set what this is.
+    /// This is where we cache the mods for the homeview.
     /// </summary>
-    internal static readonly string CachePath = BaseDirectory + @"\cache\";
+    internal static readonly string CachePath = Path.Combine(BaseDirectory, "cache");
 }
